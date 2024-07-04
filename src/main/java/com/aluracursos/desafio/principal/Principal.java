40ac766f9e0b264bd6230f4bed1d27dceabba5c5
Julieth Giraldo <156958433+JuliethGS0101@users.noreply.github.com>
2024-07-04 18:55:52 -0500

    solucion a problema

--- src/main/java/com/aluracursos/desafio/principal/Principal.java
+++ src/main/java/com/aluracursos/desafio/principal/Principal.java
@@ -4,11 +4,13 @@ import com.aluracursos.desafio.model.Datos;
 import com.aluracursos.desafio.service.ConsumoAPI;
 import com.aluracursos.desafio.service.ConvierteDatos;
 
-public class Principal {
+import java.util.Scanner;
 
+public class Principal {
     private static final String URL_BASE = "https://gutendex.com/books/";
     private ConsumoAPI consumoAPI = new ConsumoAPI();
     private ConvierteDatos conversor = new ConvierteDatos();
+    private Scanner teclado = new Scanner(System.in);
 
     public void muestraElMenu(){
         var json = consumoAPI.obtenerDatos(URL_BASE);
